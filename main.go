e1e332f2f978242f8aac40065ec2f9be80f5a4fe
Kyrylo Dudka <admin@dudbook-pro.local>
2024-07-31 20:35:38 +0300

    add db connection with related modules and dependencies

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
     "github.com/gofiber/fiber/v2"
+	"github.com/duddyV/notes-api-go-fiber/database"
 )
 
 func main() {
     // Start a new fiber app
     app := fiber.New()
 
+	database.ConnectDB()
+
     // Send a string back for GET calls to the endpoint "/"
     app.Get("/", func(c *fiber.Ctx) error {
-        err := c.SendString("And the API is UP!")
+        err := c.SendString("And the API is UP! test")
         return err
     })
 
